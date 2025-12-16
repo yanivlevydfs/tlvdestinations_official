66a14dc25a0c66b19130ed9da02d8ff0752fca9f
Yaniv Levy <63206029+yanivlevydfs@users.noreply.github.com>
2025-12-17 00:34:59 +0200

    sadsdsa

---
+++ static/js/travelpayoutscript.js
@@ -0,0 +1,6 @@
+(function () {
+  var script = document.createElement("script");
+  script.async = true;
+  script.src = "https://emrldco.com/NDYxNTYz.js?t=461563";
+  document.head.appendChild(script);
+})();
